eecf85cf0211991575dfe71cf5d63be1b942b250
midorigameee <xqbodp727@gmail.com>
2024-05-02 09:44:40 +0900

    Zeep Nambaのスケジュールを取得するAPIを作成

--- WebScapingAPI/liveCalender.py
+++ WebScapingAPI/liveCalender.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from scrapeLiveInfo import ZeepNamba
 
 app = Flask(__name__)
 
@@ -6,6 +7,13 @@ app = Flask(__name__)
 def index():
     # カレンダーを表示するためのテンプレートをレンダリング
     return render_template('calendar.html')
+
+@app.route('/api/osaka/zepp-namba', methods=['GET'])
+def getZeppNamba():
+    zn = ZeepNamba()
+    json_data = zn.getLiveInfoJson()
+    response = Response(json_data, content_type='application/json')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ WebScapingAPI/scrapeLiveInfo.py
@@ -0,0 +1,73 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+import ast
+
+class ZeepNamba:
+    def __init__(self):
+        self.url = "https://www.zepp.co.jp/hall/namba/schedule/"
+        self.area = "大阪"
+        self.site = "Zeep Namba"
+
+
+    def convertJson(self, target_date, performer, title, area, site):
+        data = {
+            "date": target_date,
+            "performer": performer,
+            "title": title,
+            "area": area,
+            "site": site
+        }
+
+        json_data = json.dumps(data, ensure_ascii=False)
+        return json_data
+
+
+    def getLiveInfoJson(self):
+        target_date = "2024/05/3"
+        performer, title = self.fetchSchedule(target_date)
+        print("CONTENT : " + performer+"「" + title + "」")
+        print("===== ===== ===== =====")
+
+        json_data = self.convertJson(target_date, performer, title, self.area, self.site)
+        return json_data
+
+
+    def createURL(self, t_year, t_month):
+        param = "?" +  "_y=" + str(t_year) + "&" + "_m=" + str(t_month)
+        url = self.url +param
+        return url
+
+
+    def fetchSchedule(self, target_date):
+        print("===== fetchSchedule =====")
+        # 日付の01を1に変換するために一度intに変換する
+        t_year, t_month, t_day = map(int, target_date.split("/"))
+
+        url = self.createURL(t_year, t_month)
+        res = requests.get(url)
+        print("URL : " + url)
+        print("CONNECTION : " + str(res.status_code))
+
+        soup = BeautifulSoup(res.text, "html.parser")
+        elems = soup.find_all("a", class_="sch-content")
+
+        for content in elems:
+            month_and_date = content.find("p", class_="sch-content-date__month")
+
+            if month_and_date is None:
+                continue
+
+            month = month_and_date.string.split(".")[0]    # タグの間のテキストコンテンツを取得するためには.stringが必要
+            date = month_and_date.string.split(".")[1]
+
+            if month == str(t_month) and date == str(t_day):
+                performer = content.find("h2", class_="sch-content-text__performer").string
+                title = content.find("h3", class_="sch-content-text__ttl").string
+
+                print("FIND RESULT : FOUND")
+                return performer, title
+
+        print("FIND RESULT : NOT FOUND")
+        print("===== ===== =====")
+        return None
