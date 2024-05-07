ace5441495284fcfa8d8da4a6336774beb2f16f7
midorigameee <xqbodp727@gmail.com>
2024-05-07 22:39:41 +0000

    Pythonの仮想環境を自動で立ち上げる設定をaliasesではなくprofileに追加する

--- .devcontainer/postStartCommand.sh
+++ .devcontainer/postStartCommand.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # bash起動時に自動でPythonの仮想環境を立ち上げる設定を追加
-echo "cd /workspaces/ScrapingLive; source ./python12_venv/bin/activate" > ~/.bash_aliases
+echo "cd /workspaces/ScrapingLive; source ./python12_venv/bin/activate" > ~/.bash_profile
