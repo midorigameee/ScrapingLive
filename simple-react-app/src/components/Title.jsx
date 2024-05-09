e772a1ea50cfe2d2325f49e08bcbc9f19e293b7b
midorigameee <xqbodp727@gmail.com>
2024-05-09 01:34:26 +0000

    タイトルを変更

--- simple-react-app/src/components/Title.jsx
+++ simple-react-app/src/components/Title.jsx
@@ -3,7 +3,7 @@ import styles from "../cssModule/Title.module.css";
 export function Title() {
   return (
     <h1 className={styles.TitleStyle}>
-      <div className={styles.TitleLine}>ライブに行こうぜ!!</div>
+      <div className={styles.TitleLine}>ライブ行こうぜ!!</div>
     </h1>
   );
 }
