a2f5adb28c04feda7e11d8ae8911f728d1dd6113
midorigameee <xqbodp727@gmail.com>
2024-05-27 00:16:35 +0000

    エリア別APIに対応する

--- simple-react-app/src/App.jsx
+++ simple-react-app/src/App.jsx
@@ -9,19 +9,29 @@ import { DateSelector } from "./components/DateSelector";
 function App() {
   const [count, setCount] = useState(0);
   const [targetDate, setTargetDate] = useState(new Date());
+  const [targetArea, setTargetArea] = useState("tokyo");
+
+  console.log(`App : ${targetArea}`);
 
   return (
     <div className={styles.AppStyle}>
       <Title />
       <div className={styles.Conditions}>
-        <AreaSelector />
+        <AreaSelector
+          targetArea={targetArea}
+          setTargetArea={(area) => setTargetArea(area)}
+        />
         <DateSelector
           targetDate={targetDate}
           setTargetDate={(date) => setTargetDate(date)}
         />
       </div>
       <CheckButton count={count} setCount={(num) => setCount(num)} />
-      <ResultViewer count={count} targetDate={targetDate} />
+      <ResultViewer
+        count={count}
+        targetDate={targetDate}
+        targetArea={targetArea}
+      />
     </div>
   );
 }
